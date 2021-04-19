38febb0bcad2088d95f9bd22523e335220632f4c
mihaibetej <betej.mihai@gmail.com>
2021-04-20 00:38:11 +0300

    Single view default project

---
+++ Tennis/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tennis
+//
+//  Created by Mihai Betej on 20/04/2021.
+//  Copyright © 2021 Mihai Betej. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
